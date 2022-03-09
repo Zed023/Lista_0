e8b1ad1457bc6f27c081e3ed6b9fcb4d9b637d1b
Zed023 <57113386+Zed023@users.noreply.github.com>
2022-03-09 20:15:11 +0100

    Add files via upload

---
+++ zadanie1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class zadanie1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Witaj!");
+        System.out.println("Dzisiaj jest: "+(new Date()));
+    }
+}
